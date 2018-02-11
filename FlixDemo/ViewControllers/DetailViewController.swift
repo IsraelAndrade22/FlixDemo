547aa64e10cd8157efef380dea75d0379eb1bdae
Israel Andrade <israel_andrade@Israels-MacBook-Pro.local>
2018-02-10 19:56:04 -0800

    Add detail view and tab bar setup

---
+++ FlixDemo/ViewControllers/DetailViewController.swift
@@ -0,0 +1,62 @@
+//
+//  DetailViewController.swift
+//  FlixDemo
+//
+//  Created by Israel Andrade on 2/7/18.
+//  Copyright © 2018 Israel Andrade. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var backDropImageView: UIImageView!
+    
+    @IBOutlet weak var posterImageVIew: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: [String: Any]?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        if let movie = movie {
+            titleLabel.text = movie["title"] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
+            overviewLabel.text = movie["overview"] as? String
+            let backdropPathString = movie["backdrop_path"] as! String
+            let posterPathString = movie["poster_path"] as! String
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            let backdropURL = URL(string: baseURLString + backdropPathString)!
+            backDropImageView.af_setImage(withURL: backdropURL)
+            let posterPathURL = URL(string: baseURLString + posterPathString)!
+            posterImageVIew.af_setImage(withURL: posterPathURL)
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
