6bd5c758b3c4496f2b4a769d12ca0b83fb4f62fb
Israel Andrade <israel_andrade@Israels-MacBook-Pro.local>
2018-02-10 20:28:54 -0800

    Add Collection View setup

---
+++ FlixDemo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  FlixDemo
+//
+//  Created by Israel Andrade on 2/10/18.
+//  Copyright © 2018 Israel Andrade. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
